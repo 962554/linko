cd7f2358954c594cef6eab99241fc6481f931a3a
962554 <254557023+962554@users.noreply.github.com>
2026-03-30 18:22:49 +0100

    refactor: use `slog` key-value pairs

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 
 	st, err := store.New(dataDir, logger)
 	if err != nil {
-		logger.Error(fmt.Sprintf("failed to create store: %v\n", err))
+		logger.Error("failed to create store", "error", err)
 		return 1
 	}
 	s := newServer(*st, httpPort, logger, cancel)
@@ -61,11 +61,11 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 
 	logger.Debug("Linko is shutting down")
 	if err := s.shutdown(shutdownCtx); err != nil {
-		logger.Error(fmt.Sprintf("failed to shutdown server: %v\n", err))
+		logger.Error("failed to shutdown server", "error", err)
 		return 1
 	}
 	if serverErr != nil {
-		logger.Error(fmt.Sprintf("server error: %v\n", serverErr))
+		logger.Error("server error", "error", serverErr)
 		return 1
 	}
 	return 0
