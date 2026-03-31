87d80d4cac8d187a52165211aa2456ececd1bffc
962554 <254557023+962554@users.noreply.github.com>
2026-03-31 15:26:00 +0100

    feat: add `build` package to track build info

---
+++ internal/build/build.go
@@ -0,0 +1,13 @@
+// -*- mode:go;mode:go-playground -*-
+// Copyright © 2026 P, Rich
+// License: MIT, see LICENSE for details
+
+// This package provides build info.
+
+package build
+
+// default build-time variables
+var (
+	GitSHA    = "unknown"
+	BuildTime = "unknown"
+)
