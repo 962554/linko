6e57248894f1dec95a53d5fd4cd22fbff485090a
962554 <254557023+962554@users.noreply.github.com>
2026-04-02 16:33:33 +0100

    feat: monitor `linko` app metrics
    Add new handler for `/metrics` using Prometheus' Go client.
    Created a new scraper job to collect the test metrics.

--- server.go
+++ server.go
@@ -13,6 +13,8 @@ import (
 
 	"962554/linko/internal/spy"
 	"962554/linko/internal/store"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type server struct {
@@ -44,6 +46,7 @@ func newServer(store store.Store, port int, logger *slog.Logger, cancel context.
 	mux.Handle("GET /api/urls", s.authMiddleware(http.HandlerFunc(s.handlerListURLs)))
 	mux.HandleFunc("GET /{shortCode}", s.handlerRedirect)
 	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
+	mux.Handle("GET /metrics", promhttp.Handler())
 
 	return s
 }
